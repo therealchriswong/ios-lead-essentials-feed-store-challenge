b0b6adc5594349c532f118ab576195063fc0e6ae
therealchriswong <chriswong20@gmail.com>
2021-05-30 07:49:00 -0400

    set cache property to optional to match FeedStore.xcdatamodel

--- FeedStoreChallenge/ManagedFeedImage.swift
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -13,7 +13,7 @@ final class ManagedFeedImage: NSManagedObject {
 	@NSManaged var imageDescription: String?
 	@NSManaged var location: String?
 	@NSManaged var url: URL
-	@NSManaged var cache: ManagedCache
+	@NSManaged var cache: ManagedCache?
 
 	static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
 		return NSOrderedSet(array: localFeed.map { local in
